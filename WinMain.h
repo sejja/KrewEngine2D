5b25dbfdca06324dad601c5708893d66642619e5
Sejja <diego.revilla.r@gmail.com>
2018-06-18 17:14:28 +0100

    Add project files.

---
+++ WinMain.h
@@ -0,0 +1,13 @@
+#ifndef _WINMAIN_H
+#define _WINMAIN_H
+
+//C Runtime Header Files
+
+#ifndef _WINDOW_
+#include <Windows.h>
+#endif
+
+int _tmain(int argc, char* argv[]);
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTA lpCmdLine, int nCmdShow);
+
+#endif //_WINMAIN_H
